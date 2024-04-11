55e47d957880b507a8258cde0f1853e96d41e3ec
edveralli <edveralli@gmail.com>
2024-04-11 18:03:04 -0300

    Emojis deleted in DF, \u codes deleted in DF

--- src/Carga_cognitiva01.py
+++ src/Carga_cognitiva01.py
@@ -44,7 +44,7 @@ df_tsv_sin_nul_sin_vacios_Active = df_tsv_sin_nul_sin_vacios[df_tsv_sin_nul_sin_
 # Lista de nombres de columnas relevantes para el analisis
 columnas_deseadas = ['Active', 'ID', 'Name','Bot Says']
 
-# Crear un nuevo DataFrame con las columnas deseadas
+# Creo un nuevo DataFrame con las columnas deseadas
 df_tsv_limpio = df_tsv_sin_nul_sin_vacios_Active[columnas_deseadas].copy()
 
 # Creo una columna nueva destino de los reemplazos
@@ -55,12 +55,46 @@ df_tsv_limpio['Bot_Says2'] = df_tsv_limpio['Bot_Says2'].str.replace(r'\${.*?}', 
 
 
 # Defino un diccionario de reemplazos
-reemplazos = {'¿': ' ','?': ' ', '!': ' ', '¡': ' ','¡': ' ',',': ' ','.': ' ','null': ' ','*': ' ','\\r\\n': ' '}
+reemplazos = {'\\t': ' ','¿': ' ','?': ' ', '!': ' ', '¡': ' ','¡': ' ',',': ' ','.': ' ','null': ' ','*': ' ','\\r\\n': ' '}
 
 # Realizo los reemplazos en una sola línea
 for buscar, reemplazar in reemplazos.items():
     df_tsv_limpio['Bot_Says2'] = df_tsv_limpio['Bot_Says2'].str.replace(buscar, reemplazar)
 
+
+"""
+Analizo los codigos de esta forma: \u2019
+"""
+import pandas as pd
+
+# Inicializar una lista para almacenar los resultados
+barra_u = []
+
+# Itero sobre cada fila del DataFrame
+for indice, fila in df_tsv_limpio.iterrows():
+    # Divido las palabras en la columna "Bot_Says2" por espacios en blanco
+    palabras = fila["Bot_Says2"].split()
+    # Verifico si alguna palabra comienza con "\u" y tiene 4 caracteres
+    for palabra in palabras:
+        if len(palabra) == 6 and palabra.startswith("\\u"):
+            barra_u.append(palabra)
+
+# Elimino elementos duplicados de la lista
+barra_u_sin_duplicados = list(set(barra_u))
+
+print("Elementos \\u reconocidos hasta el momento")
+print(barra_u_sin_duplicados)
+print("------------------------------------------------------")
+
+# Itero sobre cada fila del DataFrame 
+# reemplazo los elementos de la columna Bot_Says2
+# que correspondan a alguno de los elementos de la lista
+# barra_u_sin_duplicados
+for indice, fila in df_tsv_limpio.iterrows():
+    botisays2 = fila["Bot_Says2"]
+    for elemento in barra_u_sin_duplicados:
+        botisays2 = botisays2.replace(elemento, ' ')
+    df_tsv_limpio.at[indice, "Bot_Says2"] = botisays2
 
 
 # Agrego columna con la cantidad de palabras 
@@ -68,27 +102,33 @@ df_tsv_limpio['cant_palabras'] = df_tsv_limpio['Bot_Says2'].str.split().apply(le
 
 
 """
-Analizar como hago con los codigos de esta forma: \u2019
-"""
-
-"""
 Analizo cuantas fila del tsv contienen alguno de los emojis en formato Unicode
 Recordar que solo se pueden procesar los emojis de 1 byte, los de 2 bytes fueron descartados
 en el proceso check_emojis.py
 """
 
-# Inicializar una lista para almacenar los resultados
-resultados = []
+# Inicializo una lista para almacenar los resultados
+emojis_en_df = []
 
-# Iterar sobre cada fila del DataFrame
+# Itero sobre cada fila del DataFrame
 for indice, fila in df_tsv_limpio.iterrows():
-    # Dividir las palabras en la columna "Bot_Says2"
+    # Divido las palabras en la columna "Bot_Says2"
     palabras = fila["Bot_Says2"].split()
-    # Verificar si algún elemento buscado está presente en las palabras
+    # Verifico si algún elemento buscado está presente en las palabras
     if any(elemento in palabras for elemento in lista_emojis_unicode):
-        # Si sí, agregar la fila al DataFrame de resultados
-        resultados.append(fila)
+        # Si sí, agrego la fila al DataFrame de resultados
+        emojis_en_df.append(fila)
+        botisays2 = fila["Bot_Says2"]
+        print("--////----------------------------------------------------")
+        print("mensaje con emoji de 1byte",botisays2)
+        print("fila",indice)
+        print("--/////----------------------------------------------------")
+        for elemento2 in lista_emojis_unicode:
+            botisays2 = botisays2.replace(elemento2, ' ')
+        df_tsv_limpio.at[indice, "Bot_Says2"] = botisays2
+        
 
-# Crear un DataFrame con las filas donde hay algun emoji de la lista
-df_tsv_limpio_Solo_filas_con_Emojis = pd.DataFrame(resultados)
+
+# Creo un DataFrame con las filas donde hay algun emoji de la lista
+df_tsv_limpio_Solo_filas_con_Emojis = pd.DataFrame(emojis_en_df)
 
